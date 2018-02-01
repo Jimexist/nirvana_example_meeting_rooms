b4e428a24c6a198aa2a3f0f2d623cbdbb73df75f
Jiayu Liu <jiayu@caicloud.io>
2018-02-01 16:47:00 +0800

    define version

--- cmd/rooms/server/server.go
+++ cmd/rooms/server/server.go
@@ -2,10 +2,15 @@ package server
 
 import (
 	"github.com/caicloud/nirvana"
+	"github.com/caicloud/nirvana_example_meeting_rooms/cmd/rooms/api"
 )
 
 func Run() error {
 	serverConfig := nirvana.NewDefaultConfig("0.0.0.0", 8000)
+	serverConfig.Configure(func(c *nirvana.Config) error {
+		c.Descriptors = append(c.Descriptors, api.DefineVersion)
+		return nil
+	})
 	server := nirvana.NewServer(serverConfig)
 	return server.Serve()
 }
