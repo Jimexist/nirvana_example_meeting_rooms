835583f91372afd308b9e752d67ee29b918fd072
Jiayu Liu <jiayu@caicloud.io>
2018-02-07 11:10:11 +0800

    update id as json handler

--- pkg/api/rooms.go
+++ pkg/api/rooms.go
@@ -19,6 +19,53 @@ func GetRoomDescriptor() definition.Descriptor {
 		Path:        "/rooms",
 		Description: "rooms CRUD",
 		Produces:    []string{definition.MIMEJSON},
+		Children: []definition.Descriptor{
+			{
+				Path:        "/{id}",
+				Description: "room",
+				Definitions: []definition.Definition{
+					{
+						Method:      definition.Get,
+						Description: "get by room",
+						Parameters:  []definition.Parameter{idParam},
+						Results: []definition.Result{
+							{
+								Description: "the room info",
+								Destination: definition.Data,
+								Operators:   []definition.Operator{},
+							},
+						},
+						Function: func(ctx context.Context, ID int32) *room.Info {
+							log.Infof("getting room by ID %v", ID)
+							return nil
+						},
+					},
+					{
+						Method:      definition.Patch,
+						Description: "update room info",
+						Consumes:    []string{definition.MIMEJSON},
+						Parameters: []definition.Parameter{idParam,
+							{
+								Name:        "name",
+								Source:      definition.Body,
+								Description: "the name to update",
+							},
+						},
+						Results: []definition.Result{
+							{
+								Description: "the updated room info",
+								Destination: definition.Data,
+								Operators:   []definition.Operator{},
+							},
+						},
+						Function: func(ctx context.Context, ID int32, name string) *room.Info {
+							log.Infof("updating room %v with name %v", ID, name)
+							return nil
+						},
+					},
+				},
+			},
+		},
 		Definitions: []definition.Definition{
 			{
 				Method:      definition.List,
@@ -40,45 +87,6 @@ func GetRoomDescriptor() definition.Descriptor {
 				Function: func(ctx context.Context, name string) []room.Info {
 					log.Infof("listing rooms with name %v", name)
 					return []room.Info{}
-				},
-			},
-			{
-				Method:      definition.Get,
-				Description: "get by room",
-				Parameters:  []definition.Parameter{idParam},
-				Results: []definition.Result{
-					{
-						Description: "the room info",
-						Destination: definition.Data,
-						Operators:   []definition.Operator{},
-					},
-				},
-				Function: func(ctx context.Context, ID int32) *room.Info {
-					log.Infof("getting room by ID %v", ID)
-					return nil
-				},
-			},
-			{
-				Method:      definition.Update,
-				Description: "update room info",
-				Consumes:    []string{definition.MIMEFormData, definition.MIMEURLEncoded},
-				Parameters: []definition.Parameter{idParam,
-					{
-						Name:        "name",
-						Source:      definition.Form,
-						Description: "the name to update",
-					},
-				},
-				Results: []definition.Result{
-					{
-						Description: "the updated room info",
-						Destination: definition.Data,
-						Operators:   []definition.Operator{},
-					},
-				},
-				Function: func(ctx context.Context, ID int32, name string) *room.Info {
-					log.Infof("updating room %v with name %v", ID, name)
-					return nil
 				},
 			},
 		},
